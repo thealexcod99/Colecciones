925a6ad2f44fba2f39989b7ad3733bcf8a4f1cbe
Alejandro A. <alex@gmail.com>
2019-01-08 10:14:55 +0100

    1

--- src/teoria/coleccion5Enteros.java
+++ src/teoria/coleccion5Enteros.java
@@ -1,5 +1,6 @@
 package teoria;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class coleccion5Enteros {
@@ -17,12 +18,25 @@ public class coleccion5Enteros {
 		}
 		sc.close();
 		
-		double media = sum / TAMAÑO_COLECCION;
-		System.out.printf("El valor medio de la suma de los números introdcidos es %f%n", media);
+		System.out.println("Array original " + Arrays.toString(numeros));
 		
-		for (int i = 0; i < TAMAÑO_COLECCION; i++) {
-			
+		float media = 1.0f * sum / TAMAÑO_COLECCION;
+		System.out.printf("El valor medio de la suma de los números introdcidos es %.2f%n", media);
+		
+		//calcular el valor maximo recorriendo el array
+		int valorMaximo = Integer.MIN_VALUE; //en vez de usar un -9999999 o algo parecido
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] > valorMaximo ) {
+				valorMaximo = numeros[i];
+			}
 		}
+		System.out.printf("El valo máximo es %d%n", valorMaximo);
+		
+		//calcular el valor minimo ordenando el array
+		Arrays.sort(numeros); //ordena el array
+		System.out.println("Array ordenado " + Arrays.toString(numeros));
+		System.out.printf("El valor mínimo es %d%n", numeros[0]);
+		System.out.printf("El valor mínimo es %d%n", numeros[numeros.length - 1]);
 		
 		
 		
