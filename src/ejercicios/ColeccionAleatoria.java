a3e3198aa9a885ddb829dc6090406c3488deac48
Alejandro A. <alex@gmail.com>
2019-01-15 09:40:23 +0100

    ColeccionAleatoria finalizada

--- src/ejercicios/ColeccionAleatoria.java
+++ src/ejercicios/ColeccionAleatoria.java
@@ -1,16 +1,26 @@
 package ejercicios;
 
 import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+//import java.util.Random; 
 
 public class ColeccionAleatoria {
 	private static final int TAMANNO = 100;	
 	private static int[] array = new int[TAMANNO];
+	private int[] arrayOrdenado;
 
 	public ColeccionAleatoria() {
+		//Usar la clase random(codigo comentado del constructor)
+//		Random random = new Random();
+		
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (100 * Math.random());
+//			array[i] = random.nextInt(100);   numeros del 0 al 99 (para incluir el 100 argumentar a 101
 		}
+		arrayOrdenado = array.clone();
+		Arrays.sort(arrayOrdenado);
 	}
 	
 	public int[] getArray() {
@@ -21,8 +31,8 @@ public class ColeccionAleatoria {
 		System.out.println(Arrays.toString(array));
 	}
 	
-	public double valormedio() {
-		double suma = 0.0;
+	public double valorMedio() {
+		double suma = 0;
 		for (int i = 0; i < array.length; i++) {
 			suma += array[i];
 		}
@@ -30,39 +40,42 @@ public class ColeccionAleatoria {
 	}
 	
 	public double desviacionTipica() {
-		double suma = 0.0;
-		for (int i = 0; i < array.length; i++) {
-			suma += Math.pow(array[i] - valormedio(), 2);
+		double suma = 0;
+		double valorMedio = valorMedio();	//se asigna el valor medio a una variable para que haga una unica llamada al metodo, si se incluyera en el bucle llamaria al metodo 100 veces 
+		for (int i : array) {
+			suma += Math.pow(i - valorMedio, 2);
 		}
 		return Math.sqrt(suma / array.length);
 	}
 	
 	public int[] arrayOrdenado() {
-		int[] arrayOrdenado = array;
-		Arrays.sort(arrayOrdenado);
+//		int[] arrayOrdenado = array;
+//		Arrays.sort(arrayOrdenado);
+//		return arrayOrdenado;
+//		int[] arrayOrdenado = array.clone();	//no siempre va a funcionar el clone con todos los objetos
+//		Arrays.sort(arrayOrdenado);				//tambien se podria recorrer el array y volcarlo en el nuevo con un bucle
 		return arrayOrdenado;
 	}
 	
-//	public int[] numerosPares() {
-//		int contador = 0;
-//		int[] arrayPares = new int[contador];
-//		for (int i = 0; i < array.length; i++) {
-//			if (array[i] % 2 == 0) {
-//				contador++;
-//				arrayPares[i] = array[i];
-//			}
-//				
-//		}
-//		return arrayPares;
-//	}
-	public int[] numerosPares() {
-		ArrayList<Integer> arrayPares = new ArrayList();
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] % 2 == 0) {
-				arrayPares.add(array[i]);
+	public List<Integer> numerosPares() {
+		List<Integer> listaPares = new ArrayList();	//no siempre va a funcionar el clone con todos los objetos
+//		Arrays.sort(arrayOrdenado);
+		for (int i : array) {
+			if (i % 2 == 0) {
+				listaPares.add(i);
 			}
-
 		}
-		
+		return listaPares;
 	}
+	
+	public int valorMinimo() {
+//		int[] arrayOrdenado = arrayOrdenado();
+		return arrayOrdenado[0];
+	}
+
+	public int valorMaximo() {
+//		int[] arrayOrdenado = arrayOrdenado();
+		return arrayOrdenado[array.length -1];
+	}
+	
 }
--- src/test/TestColeccionAleatoria.java
+++ src/test/TestColeccionAleatoria.java
@@ -1,18 +1,27 @@
 package test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import ejercicios.ColeccionAleatoria;
 
 public class TestColeccionAleatoria {
 
 	public static void main(String[] args) {
-		ColeccionAleatoria coleccion = new ColeccionAleatoria();
+		ColeccionAleatoria c = new ColeccionAleatoria();
 		
-		coleccion.mostrarArray();
-		System.out.println(coleccion.valormedio());
-		System.out.printf("%.4f%n", coleccion.desviacionTipica());
-//		System.out.println(Arrays.toString(coleccion.numerosPares()));
+		int[] array = c.getArray();
+		
+		System.out.println(Arrays.toString(array));
+		System.out.printf("Valor medio %.2f%n",c.valorMedio());
+		System.out.printf("Desviación típica %.4f%n", c.desviacionTipica());
+		int[] arrayOrdenado = c.arrayOrdenado();
+		System.out.println("Original" + Arrays.toString(array));
+		System.out.println("Ordenado" + Arrays.toString(arrayOrdenado));
+		List<Integer> coleccionPares = c.numerosPares();
+		System.out.println("Pares" + coleccionPares);
+		System.out.printf("El valor mas pequeño es %d%n", c.valorMinimo());
+		System.out.printf("El valor mas grande es %d%n", c.valorMaximo());
 	}
 
 }
