7d18ce725689135880c42328353b11415e44d884
Alejandro A. <alex@gmail.com>
2019-01-17 09:34:56 +0100

    ColeccionDinamica finalizada

--- src/ejercicios/ColeccionDinamica.java
+++ src/ejercicios/ColeccionDinamica.java
@@ -5,10 +5,40 @@ import java.util.List;
 
 public class ColeccionDinamica {
 
-	private List<String> listaCadenas = new ArrayList();
+	private List<String> listaCadenas = new ArrayList<>();
 	
+	public List<String> getListaCadenas() {
+		return listaCadenas;
+	}
+
 	public void addCadena(String cadena) {
 		listaCadenas.add(cadena);
+	}
+	
+	public boolean buscarCadena(String cadena) {
+		return listaCadenas.contains(cadena);
+	}
+	
+	public void eliminarCadena(String cadena) {
+		listaCadenas.remove(cadena);
+	}
+	
+	public List<String> obtenerCadenasMayorLongitud() {
+		List<String> listaCadenasMayorLongitud = new ArrayList<>();
+		int mayorLongitud = 0;
+		
+		for (String string : listaCadenas) {
+			
+			if (string.length() > mayorLongitud) {
+				mayorLongitud = string.length();
+				listaCadenasMayorLongitud.clear();
+				listaCadenasMayorLongitud.add(string);
+			}
+			else if (string.length() == mayorLongitud) 
+				listaCadenasMayorLongitud.add(string);
+		}
+		
+		return listaCadenasMayorLongitud;
 	}
 	
 	
--- src/test/TestColeccionDinamica.java
+++ src/test/TestColeccionDinamica.java
@@ -7,17 +7,27 @@ import ejercicios.ColeccionDinamica;
 public class TestColeccionDinamica {
 
 	public static void main(String[] args) {
-		
-		ColeccionDinamica c = new ColeccionDinamica();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce las palabras que añadiras a la colección y termina con un \"fin\" o un \"FIN\"");
-		String cadena;
-		do {
-		cadena = sc.next();
-		c.addCadena(cadena);
-		}
-		while (! (sc.hasNext("FIN") || sc.hasNext("fin") || sc.hasNext("Fin")));
 
+		ColeccionDinamica c = new ColeccionDinamica();
+		System.out.println("Introduce palabras. Para acabar usa fin");
+		Scanner sc = new Scanner(System.in);
+
+		String cadena = sc.next();
+		if (!cadena.equalsIgnoreCase("fin"))
+			c.addCadena(cadena);
+		
+		while (!cadena.equalsIgnoreCase("fin") && sc.hasNext()) {
+			cadena = sc.next();
+			if (!cadena.equalsIgnoreCase("fin"))
+				c.addCadena(cadena);
+		}
+		
+		sc.close();
+		c.eliminarCadena("uno");
+		System.out.printf("Lista de cadenas de mayor longitud%n%s%n", c.obtenerCadenasMayorLongitud());
+		System.out.println(c.getListaCadenas());
+
+		
 	}
 
 }
